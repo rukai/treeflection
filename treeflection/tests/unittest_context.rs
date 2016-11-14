51385942cd233fdd89fa8734d79369e57c1fc433
Rukai <rubickent@gmail.com>
2016-11-14 21:45:50 +1100

    Add ContextVec with unittests

---
+++ treeflection/tests/unittest_context.rs
@@ -0,0 +1,160 @@
+#[macro_use] extern crate treeflection;
+#[macro_use] extern crate matches;
+extern crate serde;
+
+use treeflection::{Node, NodeRunner, NodeToken, ContextVec};
+use serde::{Serialize, Deserialize};
+
+fn test_vec() -> ContextVec<i32> {
+    ContextVec::from_vec(vec!(10, 1337, 42))
+}
+
+fn assert_context_run<T>(context_vec: &mut ContextVec<T>, result: &str) where T: Node + Serialize + Deserialize {
+    let runner = NodeRunner { tokens: vec!(
+        NodeToken::Get,
+        NodeToken::ChainContext
+    )};
+    assert_eq!(result, context_vec.node_step(runner).as_str());
+}
+
+#[test]
+fn test_chain_context() {
+    let mut context_vec = test_vec();
+    assert_context_run(&mut context_vec, "|");
+
+    context_vec.set_context(0);
+    assert_context_run(&mut context_vec, "|10|");
+
+    context_vec.set_context(1);
+    assert_context_run(&mut context_vec, "|1337|");
+
+    context_vec.set_context(2);
+    assert_context_run(&mut context_vec, "|42|");
+
+    context_vec.set_context_vec(vec!(2, 0));
+    assert_context_run(&mut context_vec, "|42|10|");
+
+    context_vec.set_context_vec(vec!(0, 1, 2));
+    assert_context_run(&mut context_vec, "|10|1337|42|");
+}
+
+#[test]
+fn test_clear_context() {
+    let mut context_vec = test_vec();
+    context_vec.set_context(0);
+    context_vec.clear_context();
+    assert_eq!(context_vec.get_context().len(), 0);
+}
+
+#[test]
+fn test_set_context() {
+    let mut context_vec = test_vec();
+
+    context_vec.set_context(0);
+    assert_eq!(context_vec.get_context().len(), 1);
+    assert_eq!(context_vec.get_context()[0], 0);
+
+    context_vec.set_context(1);
+    assert_eq!(context_vec.get_context().len(), 1);
+    assert_eq!(context_vec.get_context()[0], 1);
+
+    context_vec.set_context(1);
+    assert_eq!(context_vec.get_context().len(), 1);
+    assert_eq!(context_vec.get_context()[0], 1);
+
+    context_vec.set_context(2);
+    assert_eq!(context_vec.get_context().len(), 1);
+    assert_eq!(context_vec.get_context()[0], 2);
+}
+
+#[test]
+#[should_panic]
+fn test_set_context_out_of_bounds() {
+    let mut context_vec = test_vec();
+    context_vec.set_context(3);
+}
+
+#[test]
+fn test_set_context_vec() {
+    let mut context_vec = test_vec();
+
+    context_vec.set_context_vec(vec!(1));
+    assert_eq!(context_vec.get_context().len(), 1);
+    assert_eq!(context_vec.get_context()[0], 1);
+
+    context_vec.set_context_vec(vec!(1, 2));
+    assert_eq!(context_vec.get_context().len(), 2);
+    assert_eq!(context_vec.get_context()[0], 1);
+    assert_eq!(context_vec.get_context()[1], 2);
+
+    context_vec.set_context_vec(vec!(2, 1));
+    assert_eq!(context_vec.get_context().len(), 2);
+    assert_eq!(context_vec.get_context()[0], 2);
+    assert_eq!(context_vec.get_context()[1], 1);
+}
+
+#[test]
+fn test_set_vec() {
+    let mut context_vec = test_vec();
+    context_vec.set_context(0);
+    context_vec.set_vec(vec!(1, 99));
+    assert_eq!(context_vec.get_context().len(), 0);
+    let vec = context_vec.get_vec();
+    assert_eq!(vec[0], 1);
+    assert_eq!(vec[1], 99);
+}
+
+#[test]
+fn test_push() {
+    let mut context_vec = test_vec();
+    assert_eq!(context_vec.get_vec().len(), 3);
+    context_vec.push(99);
+    assert_eq!(context_vec.get_vec().len(), 4);
+    assert_eq!(context_vec.get_vec()[3], 99);
+}
+
+#[test]
+fn test_insert() {
+    let mut context_vec = test_vec();
+    context_vec.set_context_vec(vec!(0, 1, 2));
+
+    assert_eq!(context_vec.get_context().len(), 3);
+    assert_eq!(context_vec.get_vec().len(), 3);
+    context_vec.insert(3, 99);
+    assert_eq!(context_vec.get_context().len(), 3);
+    assert_eq!(context_vec.get_context()[0], 0);
+    assert_eq!(context_vec.get_context()[1], 1);
+    assert_eq!(context_vec.get_context()[2], 2);
+    assert_eq!(context_vec.get_vec().len(), 4);
+    assert_eq!(context_vec.get_vec()[0], 10);
+    assert_eq!(context_vec.get_vec()[1], 1337);
+    assert_eq!(context_vec.get_vec()[2], 42);
+    assert_eq!(context_vec.get_vec()[3], 99);
+
+    context_vec.insert(1, 101);
+    assert_eq!(context_vec.get_context().len(), 3);
+    assert_eq!(context_vec.get_context()[0], 0);
+    assert_eq!(context_vec.get_context()[1], 2);
+    assert_eq!(context_vec.get_context()[2], 3);
+    assert_eq!(context_vec.get_vec().len(), 5);
+    assert_eq!(context_vec.get_vec()[0], 10);
+    assert_eq!(context_vec.get_vec()[1], 101);
+    assert_eq!(context_vec.get_vec()[2], 1337);
+    assert_eq!(context_vec.get_vec()[3], 42);
+    assert_eq!(context_vec.get_vec()[4], 99);
+}
+
+#[test]
+fn test_pop() {
+    let mut context_vec = ContextVec::<bool>::new();
+    assert!(matches!(context_vec.pop(), None));
+
+    let mut context_vec = test_vec();
+    context_vec.set_context_vec(vec!(1, 2));
+
+    assert_eq!(context_vec.get_context().len(), 2);
+    assert_eq!(context_vec.get_vec().len(), 3);
+    assert!(matches!(context_vec.pop(), Some(42)));
+    assert_eq!(context_vec.get_context().len(), 1);
+    assert_eq!(context_vec.get_vec().len(), 2);
+}
