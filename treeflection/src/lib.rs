b4693c644eb47faeac19a4a87257b3dabce2bb10
Lucas Kent <rubickent@gmail.com>
2019-01-18 14:35:34 +1100

    Uniform paths and update rust version

--- treeflection/src/lib.rs
+++ treeflection/src/lib.rs
@@ -1,10 +1,10 @@
 #[macro_use] extern crate serde_derive;
 
-pub use crate::node_runner::NodeRunner;
-pub use crate::node::Node;
-pub use crate::context_vec::ContextVec;
-pub use crate::keyed_context_vec::KeyedContextVec;
-pub use crate::node_token::NodeToken;
+pub use node_runner::NodeRunner;
+pub use node::Node;
+pub use context_vec::ContextVec;
+pub use keyed_context_vec::KeyedContextVec;
+pub use node_token::NodeToken;
 
 pub mod node;
 pub mod node_runner;
